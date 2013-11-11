5b26f7d4a636f3b90e181d9fa51ed9d4f616d920
carolineclark <caroline@kano.me>
2013-11-11 16:48:24 +0000

    Replaced pi references with $USER, added $ to variable names

--- bin/make-music.sh
+++ bin/make-music.sh
@@ -8,13 +8,13 @@
 APPNAME="Make Music - default"
 DIALOG_TITLE="Make Music"
 PIDFILE=/tmp/start-make-music.pid
+WORKSPACE_ROOT_DIR=/home/$USER/.make-music
 
 # Workspaces to be created first time Make-Music is launched
 function init() 
 {
-    WORKSPACE_ROOT_DIR=/home/pi/.make-music
     WORKSPACE_DIR=$WORKSPACE_ROOT_DIR/workspaces/default
-    CONTENT_DIR=/home/pi/Music-content
+    CONTENT_DIR=/home/$USER/Music-content
 
     echo "Creating workspace directories"
     mkdir -pv $WORKSPACE_DIR/one
@@ -50,7 +50,7 @@ function cleanup {
 trap cleanup EXIT
 
 # First time Make-Music is launched, call init() 
-if [! -f WORKSPACE_DIR]; then
+if [! -f $WORKSPACE_DIR]; then
     init
 fi
 

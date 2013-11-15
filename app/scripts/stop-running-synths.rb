33169ec2faf9607bb47b5a0781afeaebad293aa9
tombettany <tom.bettany@gmail.com>
2013-11-15 15:02:16 +0000

    Changed all references to 'localhost' to 127.0.0.1 to fix bug #9

--- app/scripts/stop-running-synths.rb
+++ app/scripts/stop-running-synths.rb
@@ -3,5 +3,5 @@ $:.unshift File.expand_path("../vendor/osc-ruby/lib", __FILE__)
 require 'osc-ruby'
 
 SYNTH_GROUP = 1
-client = OSC::Client.new('localhost', 4556)
+client = OSC::Client.new('127.0.0.1', 4556)
 client.send(OSC::Message.new("/g_freeAll", SYNTH_GROUP))

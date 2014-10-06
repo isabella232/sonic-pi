e36d51f99c0acbd340a4b11a644261c98f8cd622
Tom Bettany <tom@kano.me>
2014-10-06 18:00:28 +0100

    Ensures that everything is killed on exit.

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1283,6 +1283,11 @@ void MainWindow::onExitCleanup()
     sendOSC(msg);
   }
   osc_thread.waitForFinished();
+
+  // Ensure that all the processes are killed
+  system("pkill ruby");
+  system("pkill scsynth");
+
   std::cout << "Exiting..." << std::endl;
 
 }

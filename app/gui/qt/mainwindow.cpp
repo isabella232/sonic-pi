7a4758a3c10b5dab60f5437c567c091745d5a536
Tom Bettany <tom@kano.me>
2014-10-06 17:09:38 +0100

    Opens Sonic Pi maximised.

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -351,7 +351,7 @@ MainWindow::MainWindow(QApplication &app, QSplashScreen &splash, std::string loa
   closeInfoAct->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_W));
   connect(closeInfoAct, SIGNAL(triggered()), this, SLOT(about()));
   infoWidg->addAction(closeInfoAct);
-  this->showNormal();
+  this->showMaximized();
 
 
 }

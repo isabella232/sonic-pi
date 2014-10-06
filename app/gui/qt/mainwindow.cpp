9a6979aa86247a3d779333bdfd776ded6710db34
Tom Bettany <tom@kano.me>
2014-10-06 15:32:25 +0100

    Consistently positions the dialog windows.

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1162,7 +1162,7 @@ bool MainWindow::saveAs()
 
 bool MainWindow::saveDialog()
 {
-    SaveDialog * save_dialog = new SaveDialog();
+    SaveDialog * save_dialog = new SaveDialog(this);
     save_dialog->set_file_contents(getCurrentWorkspace()->text().toUtf8().constData());
 
     save_dialog->exec();
@@ -1174,7 +1174,7 @@ bool MainWindow::saveDialog()
 
 bool MainWindow::shareDialog()
 {
-    ShareDialog * share_dialog = new ShareDialog();
+    ShareDialog * share_dialog = new ShareDialog(this);
     share_dialog->set_file_contents(getCurrentWorkspace()->text().toUtf8().constData());
 
     share_dialog->exec();
@@ -1190,7 +1190,7 @@ QsciScintilla * MainWindow::getCurrentWorkspace() {
 
 void MainWindow::load()
 {
-    LoadSourceDialog * load_from_dialog = new LoadSourceDialog();
+    LoadSourceDialog * load_from_dialog = new LoadSourceDialog(this);
     int rc = load_from_dialog->exec();
 
     if (rc == QDialog::Accepted) {

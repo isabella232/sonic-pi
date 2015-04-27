f6046ff9550c635f272799623216ece48974537c
Tom Bettany <tom@kano.me>
2015-04-27 13:55:41 +0100

    Sample Recording: Move the logic to the server
    A sample should be recorded every time the track is played. Originally
    this was done from calls within the GUI but it makes more sense to have
    them happen in the server function itself.
    
    This has the added benefit that the function for sending messages to the
    server is no longer used from within a thread, so no longer needs to be
    static.

--- app/gui/qt/mainwindow.h
+++ app/gui/qt/mainwindow.h
@@ -203,7 +203,7 @@ private:
     std::string number_name(int);
     std::string workspaceFilename(SonicPiScintilla* text);
     SonicPiScintilla* filenameToWorkspace(std::string filename);
-    static void sendOSC(oscpkt::Message m);
+    void sendOSC(oscpkt::Message m);
     void initPrefsWindow();
     void initDocsWindow();
     void refreshDocContent();
@@ -223,7 +223,6 @@ private:
     QString readFile(QString name);
     QString rootPath();
     QsciScintilla * getCurrentWorkspace();
-    static void * record_sample(void *);
 
     void addUniversalCopyShortcuts(QTextEdit *te);
 

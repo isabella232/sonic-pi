c2310191396988f4719389ca3538d11112b5e55b
Tom Bettany <tom@kano.me>
2015-07-24 14:31:32 +0100

    Sample: Remove MP3 sample code and Lame dependency
    KanoComputing/peldins#1691
    Ensure that all of the offending MP3 code is removed and all
    dependencies on non-free MP3 library packages is removed.

--- app/gui/qt/audio_tools.cpp
+++ app/gui/qt/audio_tools.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <cstring>
 
-#include <lame/lame.h>
 #include <sndfile.h>
 
 #include "audio_tools.h"
@@ -89,59 +88,6 @@ int AudioTools::convert_wav_to_ogg(std::string src_path, std::string dst_path)
 
     sf_close(src_file);
     sf_close(dst_file);
-
-    return 0;
-}
-
-
-int AudioTools::convert_wav_to_mp3(std::string src_path, std::string dst_path) {
-    /**
-     * Convert a wav file to mp3 using the Lame library
-     */
-
-    FILE * src = fopen(src_path.c_str(), "rb");
-
-    if (!src)
-        return -1;
-
-    FILE * dst = fopen(dst_path.c_str(), "wb");
-
-    const int PCM_SIZE = 8192;
-    const int MP3_SIZE = 8192;
-
-    short int pcm_buffer[PCM_SIZE * 2];
-    unsigned char mp3_buffer[MP3_SIZE];
-
-    // Lame config
-    lame_t lame = lame_init();
-
-    lame_set_in_samplerate(lame, 44100);
-    lame_set_VBR(lame, vbr_default);
-    lame_set_num_channels(lame, 2);
-
-    lame_init_params(lame);
-
-    int read, write;
-
-    // Convert
-    do {
-        read = fread(pcm_buffer, 2 * sizeof(short int),
-                     PCM_SIZE, src);
-
-        if (read == 0)
-            write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
-        else
-            write = lame_encode_buffer_interleaved(lame, pcm_buffer, read,
-                                                   mp3_buffer, MP3_SIZE);
-
-        fwrite(mp3_buffer, write, 1, dst);
-    } while (read != 0);
-
-    // Clean up
-    lame_close(lame);
-
-    fclose(src);
-    fclose(dst);
 
     return 0;
 }
--- app/gui/qt/audio_tools.h
+++ app/gui/qt/audio_tools.h
@@ -1,6 +1,5 @@
 #include <string>
 
 namespace AudioTools {
-    int convert_wav_to_mp3(std::string src_path, std::string dst_path);
     int convert_wav_to_ogg(std::string src_path, std::string dst_path);
 }

442aad4904df3b94f285bf9775675d6c7ea6bead
Albert Casals <skarbat@gmail.com>
2016-01-29 13:47:40 +0100

    Adding a new dbus endpoint to load a share passing a filename
     * A new dbus function load_share accepting a string
     * increased version
     * added missing build dependency package

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1262,6 +1262,12 @@ void MainWindow::load()
     delete load_from_dialog;
 }
 
+void MainWindow::load_share(const QString share_filename)
+{
+    // TODO: unfold the file into the workspace.
+    getCurrentWorkspace()->setText(share_filename);
+}
+
 void MainWindow::sendOSC(Message m)
 {
   int TIMEOUT = 30000;
